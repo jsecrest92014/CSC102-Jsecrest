1d37fc1c5295f177303866148fd77491f17bcacb
jsecrest92014 <secrestworks@gmail.com>
2025-04-29 20:29:58 -0500

    Add files via upload

--- code.js
+++ code.js
@@ -1,5 +1,6 @@
 console.log("Game Page");
 // Changes Text in Header
+//J. Secrest
 function updateText(){
     document.getElementById("rookie").innerHTML = "Slowly but Surely"
 }
@@ -13,28 +14,70 @@ function playGame()
     let die2 = Math.floor(Math.random() * 6) + 1;
 // in "" displays in console log followed by the number that is generated randomly in this string on the first line
     console.log("die 1:" + die1);
-     
+    
 // in "" displays in console log followed by the number generated randomly in this on the 2nd line 
     console.log("die 2:" + die2);
-  
+    
     let sum = die1 + die2; 
+    
 // OR operator to include sum of 11
     if(sum == 7 || sum == 11)
     {
         console.log("Craps- You Lose!");
-        results.innerHTML = "Craps - You Lose!";
+        results.innerHTML = "Craps - You Lose!   " + ("die1: " + die1) + ("  / die2:   " + die2);
     }
     // AND operator set for even number
     else if(die1 == die2 && die1 %2 == 0)
     {
         console.log("You Won")
+        results.innerHTML = "You Win!   " + ("die1: " + die1) + (" /  die2:   " + die2)
     }
     else
     {
+        // results displayed in console log and on inner html
         console.log("You Pushed!")
-        results.innerHTML = "You Pushed!";
+        results.innerHTML = "You Pushed!   " + ("die1: " + die1) + ("  / die2:   " + die2);
+        
     }
 
 } 
+function palindrome()
+{
+    // Palindrome identification // 
+    console.log("PalindromeVerification");
+    let inputField = document.getElementById("palindromeblock");
 
+    let results = document.getElementById("palindromeResults");
+   
+    let inputValue = inputField.value;
+
+    let stringLength = inputValue.length;
+
+    if (stringLength > 0)
+    {
+        let iterationCount = Math.floor(stringLength/2);
+        
+        let lastCharacterIndex = inputValue.length -1;
+
+        let palindromeResults = true;
+
+        // proceed if palindrome is verified
+        for(let i = 0; i < iterationCount; i++)
+        {
+            if (inputValue[i] !== inputValue[lastCharacterIndex - i])
+            {
+                console.log("Not a Palindrome");
+                results.innerHTML = "No Go Amigo"
+                palindromeResults = false;
+                break;
+            }
+
+        }
+        if (palindromeResults)
+            {
+            console.log("This is a Palindrome")
+            results.innerHTML = "Palindrome!!"
+            }
+    }
+}
    
